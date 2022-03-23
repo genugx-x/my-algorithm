875c4b27f099391fb077e7a34d1d07994e4263bc
sb.kang <qparanoya@gmail.com>
2022-03-23 17:49:07 +0900

    코딩테스트 연습 > 완전탐색 > 모의고사 풀이 중 커밋

--- programmers/src/test/java/com/genug/programmers/bruteforce/lv1/MockExam.java
+++ programmers/src/test/java/com/genug/programmers/bruteforce/lv1/MockExam.java
@@ -3,6 +3,7 @@ package com.genug.programmers.bruteforce.lv1;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,27 +35,50 @@ public class MockExam {
     int[] solution(int[] answers) {
         int[] answer = null;
 
-        int[] giver1 = new int[] {1, 2, 3, 4, 5};
+        MathGiver mathGiver = new MathGiver(1, 0, new int[] {1, 2, 3, 4, 5});
         int[] giver2 = new int[] {2, 1, 2, 3, 2, 4, 2, 5};
         int[] giver3 = new int[] {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        List<Integer> answerCounts = new ArrayList<>();
-        answerCounts.add(0);
-        answerCounts.add(0);
-        answerCounts.add(0);
+        /*
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == giver1[i % giver1.length]) {
-                answerCounts.set(0, answerCounts.get(0)+1);
+                answerCounts[0] = answerCounts[0]+1;
             }
             if (answers[i] == giver2[i % giver2.length]) {
-                answerCounts.set(1, answerCounts.get(1)+1);
+                answerCounts[1] = answerCounts[1]+1;
             }
             if (answers[i] == giver3[i % giver3.length]) {
-                answerCounts.set(2, answerCounts.get(2)+1);
+                answerCounts[2] = answerCounts[2]+1;
             }
         }
 
-
+        Arrays.sort(answerCounts);
+        System.out.println(Arrays.toString(answerCounts));
+        int answerMaxCount = 0;
+        for (int i = answerCounts.length - 1; i >= 0; i--) {
+            if (answerCounts[answerCounts.length - 1] == answerCounts[i]) {
+            }
+        }
+        answer = new int[answerMaxCount];
+        for (int i = answerCounts.length - 1; i >= 0; i--) {
+            if (answerCounts[answerCounts.length - 1] == answerCounts[i]) {
+                answerMaxCount += 1;
+            }
+        }
+        
+         */
         return answer;
+    }
+
+    static class MathGiver {
+        Integer number;
+        Integer answerCount;
+        int[] answers;
+
+        public MathGiver(int number, int answerCount, int[] answers) {
+            this.number = number;
+            this.answerCount = answerCount;
+            this.answers = answers;
+        }
     }
 }
