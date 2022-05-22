5d8eae9a73f7156147d507ad0b70f16034032591
genugx_x <qparanoya@gmail.com>
2022-05-22 23:20:21 +0900

    백준 문제풀이 추가 - 2609 (최대공약수, 최대공배수) 풀이 코드

--- baekjoon/src/main/java/org/genug/algorithm/Main.java
+++ baekjoon/src/main/java/org/genug/algorithm/Main.java
@@ -5,47 +5,37 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int testCaseCount = Integer.parseInt(scanner.nextLine());
-        while (testCaseCount > 0) {
-            String[] line = scanner.nextLine().split("\\s+");
-            int n = Integer.parseInt(line[0]);
-            int m = Integer.parseInt(line[1]);
-            line = scanner.nextLine().split("\\s+");
-            Queue<Document> queue = new LinkedList<>();
-            Queue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-            for (int i = 0; i < n; i++) {
-                int p = Integer.parseInt(line[i]);
-                queue.add(new Document(p, i == m));
-                priorityQueue.add(p);
-            }
-
-            int result = 0;
-            while (!priorityQueue.isEmpty()) {
-                int i = priorityQueue.peek(); // 출력되어야 할 문서 중요도
-                Document document = queue.poll();
-                if (document.importance == i) {
-                    priorityQueue.poll();
-                    result++;
-                    if (document.flag)
-                        break;
-                } else {
-                    queue.add(document);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int max = 0;
+        int min = 0;
+        if (n > m) {
+            max = n;
+            min = m;
+        } else {
+            max = m;
+            min = n;
+        }
+        for (int i = min; i > 0; i--) {
+            if (min % i == 0) {
+                int t = min / i;
+                if (max % i == 0) {
+                    System.out.println("최대공약수 : " + i);
+                    break;
                 }
             }
-
-            System.out.println(result);
-            testCaseCount--;
         }
 
-    }
-
-    static class Document {
-        int importance;
-        boolean flag;
-        public Document (int importance, boolean flag) {
-            this.importance = importance;
-            this.flag = flag;
+        int count = 1;
+        while (true) {
+            int multiple = count * max;
+            if (multiple % min == 0) {
+                System.out.println("최소공배수 : " + multiple);
+                break;
+            }
+            count++;
         }
+
     }
 
 }
--- baekjoon/src/main/java/org/genug/algorithm/cls/Class2.java
+++ baekjoon/src/main/java/org/genug/algorithm/cls/Class2.java
@@ -430,4 +430,40 @@ public class Class2 {
         }
     }
 
+    // 2609 - 최대공약수 최대공배수
+    public void ex_2609() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int max = 0;
+        int min = 0;
+        if (n > m) {
+            max = n;
+            min = m;
+        } else {
+            max = m;
+            min = n;
+        }
+        for (int i = min; i > 0; i--) {
+            if (min % i == 0) {
+                int t = min / i;
+                if (max % i == 0) {
+                    System.out.println("최대공약수 : " + i);
+                    break;
+                }
+            }
+        }
+
+        int count = 1;
+        while (true) {
+            int multiple = count * max;
+            if (multiple % min == 0) {
+                System.out.println("최소공배수 : " + multiple);
+                break;
+            }
+            count++;
+        }
+
+    }
+
 }
