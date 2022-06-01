74d9eeac041d741df52d15c388373b8ff2f5ebec
genugx_x <qparanoya@gmail.com>
2022-06-01 10:48:12 +0900

    백준 풀이 코드 추가 - 2606 (바이러스) [풀이 완료]

--- baekjoon/src/main/java/org/genug/algorithm/Main.java
+++ baekjoon/src/main/java/org/genug/algorithm/Main.java
@@ -5,5 +5,50 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int m = Integer.parseInt(scanner.nextLine());
+        Computer[] computers = new Computer[n+1];
+        for (int i = 1; i < computers.length; i++) {
+            computers[i] = new Computer(i);
+        }
+        while (m > 0) {
+            String[] inputs = scanner.nextLine().split("\\s+");
+            int a = Integer.parseInt(inputs[0]);
+            int b = Integer.parseInt(inputs[1]);
+
+            if (!computers[a].networks.contains(b))
+                computers[a].networks.add(b);
+            if (!computers[b].networks.contains(a))
+                computers[b].networks.add(a);
+            m--;
+        }
+
+        int result = dfs(1, computers)-1; // 1번 제외
+        System.out.println(result);
+    }
+
+    public static int dfs(int n, Computer[] computers) {
+        int result = 1;
+        Computer com = computers[n];
+        com.visitFlag = true;
+        for (int i = 0; i < com.networks.size(); i++) {
+            int network = com.networks.get(i);
+            if (!computers[network].visitFlag)
+                result += dfs(network, computers);
+        }
+        return result;
+    }
+
+    static class Computer {
+        Integer number;
+        Boolean visitFlag;
+        List<Integer> networks;
+
+        public Computer(int number) {
+            this.number = number;
+            this.visitFlag = false;
+            this.networks = new ArrayList<>();
+        }
     }
 }
--- baekjoon/src/main/java/org/genug/algorithm/cls/Class3.java
+++ baekjoon/src/main/java/org/genug/algorithm/cls/Class3.java
@@ -1,10 +1,5 @@
 package org.genug.algorithm.cls;
 
-import org.genug.algorithm.Main;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Class3 {
