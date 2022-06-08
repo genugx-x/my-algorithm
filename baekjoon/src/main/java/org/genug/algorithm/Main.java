40c19028ad7bbd5fffa942b641c9865af4c32b37
genugx_x <qparanoya@gmail.com>
2022-06-08 22:15:47 +0900

    백준 풀이 중 코드 추가 - 2573 (빙산)

--- baekjoon/src/main/java/org/genug/algorithm/Main.java
+++ baekjoon/src/main/java/org/genug/algorithm/Main.java
@@ -8,8 +8,23 @@ public class Main {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
-            int n = Integer.parseInt(br.readLine());
-        } catch (IOException ignored) {
+            String[] input = br.readLine().split("\\s+");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+
+            int[][] arr = new int[n][m];
+            boolean[][] visited = new boolean[n][m];
+
+            for (int i = 0; i < arr.length; i++) {
+                input = br.readLine().split("\\s+");
+                for (int j = 0; j < arr[i].length; j++) {
+                    arr[i][j] = Integer.parseInt(input[j]);
+                }
+            }
+
+            // 1 분
+
+        }catch (IOException ignored) {
         } finally {
             try { br.close(); } catch (IOException ignored) {}
         }
